f5f17daa0b1b9040ee37848cf3cc7252a31bfeda
Christoph Flügel <christoph@fluegel.it>
2021-11-12 13:17:37 +0100

    minor fixes and improvments in the preseed and latecommand script file

--- late_command.sh
+++ late_command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 
 
-mkdir /root/.ssh
+mkdir -p /root/.ssh 
 touch /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOILVdZdgxXq+X7Lv7Gz617aPP9H8tqaOpJvtmITKQUS ansible key for automatic deployments" > /root/.ssh/authorized_keys 
